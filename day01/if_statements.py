122cb886582b96a129c86636009694f2dde90bb0
azatbayram <bayramov.azat.tkm@gmail.com>
2023-09-18 23:15:18 +0500

    give a grade example with if statement

--- day01/if_statements.py
+++ day01/if_statements.py
@@ -78,3 +78,21 @@ if score >=0 and score <=100:
         print('Failed')
 else:
     print("not valid score")
+
+print("------------------------------------------")
+
+score = 95
+
+if 0 <= score <= 100:
+    if score >= 90:
+        print('A')
+    elif score >= 80:
+        print('B')
+    elif score >= 70:
+        print('C')
+    elif score >= 60:
+        print('D')
+    else:
+        print('F')
+else:
+    print('invalid score')
