6843774db92c6c603087dc264c41c181a4ccc87a
azatbayram <bayramov.azat.tkm@gmail.com>
2023-09-18 22:01:00 +0500

    bootcamp started

---
+++ day01/if_statements.py
@@ -0,0 +1,80 @@
+if False:
+    print('Python Programming')
+
+print('Java Programming')
+
+print('--------------------------')
+
+score = 70
+
+if score >= 60:
+    print('Congrats! You passed the exam')
+
+"""
+if(true){
+
+}
+"""
+
+s = 'Hello World'
+
+if 'H' and "W" in s:
+    print(s, 'has', 'H and W')
+
+print("------------------------------------------")
+
+if score >= 60:
+    print('Passed')
+else:
+    print('Failed')
+
+print("------------------------------------------")
+
+result = None
+age = 20
+
+if age >=21:
+    result = 'eligible'
+else:
+    result = 'not eligible'
+
+print(result)
+
+print("------------------------------------------")
+
+# Ternary
+
+result='eligible' if score >= 21 else 'not eligible'
+
+print(result)
+print("------------------------------------------")
+
+num = 100
+result = None
+
+if num > 0:
+    result = 'positive'
+elif num < 0:
+    result = 'negative'
+else:
+    pass
+
+print(result)
+
+print("------------------------------------------")
+
+result_2 = 'positive' if num > 0 else 'negative'
+
+print(result_2)
+
+print("------------------------------------------")
+
+score = -300
+
+if score >=0 and score <=100:
+    if score >=60:
+        print('Passed')
+    else:
+        print('Failed')
+else:
+    print("not valid score")
---
+++ day01/operators.py
@@ -0,0 +1,60 @@
+# arithmetic : +, -, *, /, %
+
+print(10-2)
+print(10+2)
+
+print(10*3)
+
+print(10/3)
+print(10/2)
+
+print(10%3)
+
+print(int(100/2))
+
+print("------------------------------------------")
+# shorthand assignment operators: =, +=, -=, /=, %=
+
+a=100
+
+a+=100
+print(a)
+
+a-=100
+print(a)
+
+a*=3
+print(a)
+
+a/=2
+print(a)
+
+print("------------------------------------------")
+# logica operators: and, or, not
+
+str='Hello World'
+
+print('H' and 'W' in str)
+
+print(True and True)
+print(True and False)
+print(True or False)
+
+str='Java Python C# C++'
+print('Java' or 'Ruby' in str)
+
+age=28
+citizen_ship='USA'
+is_eligible= age>=18 and citizen_ship=='USA'
+
+print(is_eligible)
+
+has_Java='Java' in str
+print(has_Java)
+
+print('Python' not in str)
+
+print(not True)
+print(not False)
+
+
