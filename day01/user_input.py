41486db71f756e5b64709756660858e406c81f90
azatbayram <bayramov.azat.tkm@gmail.com>
2023-09-18 23:46:01 +0500

    read user input by using input() method

---
+++ day01/user_input.py
@@ -0,0 +1,15 @@
+# print(help(input))
+
+name = input('Enter your name:\n')
+age = int(input('Enter your age:\n'))
+boolean_value = bool(input('Enter True or False:\n'))
+
+print(type(name))
+print(name)
+
+print(type(age))
+print(age)
+
+print(type(boolean_value))
+print(boolean_value)
+
