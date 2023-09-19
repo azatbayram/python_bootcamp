b1b7452d1f2b76527818b3bb7a5973072fb6c925
azatbayram <bayramov.azat.tkm@gmail.com>
2023-09-20 02:44:51 +0500

    create square file

---
+++ day01/square.py
@@ -0,0 +1,5 @@
+side = int(input('Enter square side size: '))
+area = side * side
+perimeter = side * 4
+print(f'Area of the square is equal to {area}')
+print(f'Perimeter of square is equal to {perimeter}')
