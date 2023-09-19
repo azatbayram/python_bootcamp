c1f5da5a5e6546f836f16602a6b5aae5281e3757
azatbayram <bayramov.azat.tkm@gmail.com>
2023-09-20 03:19:16 +0500

    updated employee_info and ShippingAddress files

--- day01/ShippingAddress.py
+++ day01/ShippingAddress.py
@@ -6,3 +6,12 @@ state = "VA"
 zip_code = 22030
 
 print(f'Your shipping address is:\n\t\t{name}\n\t\t{building_number} {street_name}\n\t\t{city}, {state} {zip_code}')
+
+name = input("1. \"Enter your full name\"")
+building_number = input('2. \"Enter your building name\"')
+street_name = input('3. \" Enter your street name\"')
+city = input('4. \"Enter your city name\"')
+state = input('5. \"Enter your state name\"')
+zip_code = input('6. \"Enter your zip code')
+
+print(f'Your shipping address is:\n\t\t{name}\n\t\t{building_number} {street_name}\n\t\t{city}, {state} {zip_code}')
--- day01/employee_info.py
+++ day01/employee_info.py
@@ -9,3 +9,14 @@ is_FullTime = True
 is_Married = False
 employee_id = 12345
 
+name = input('Enter name:')
+age = int(input('Enter age:'))
+gender = input('Enter gender:')
+company = input('Enter company name:')
+jobTitle = input('Enter job title:')
+salary = float(input('Enter your salary:'))
+
+print(f'{name} is {age} years old, gender is {gender}\n'
+      f'{name} works at {company} as a {jobTitle}\n'
+      f'{name} makes $ {salary} per year')
+
