533ab5622064fa5cbe3148e2202f17ad10b21815
azatbayram <bayramov.azat.tkm@gmail.com>
2023-09-20 01:13:06 +0500

    create ShippingAddress file

---
+++ day01/ShippingAddress.py
@@ -0,0 +1,8 @@
+name = "Aaron Kissinger"
+building_number = '13621A'
+street_name = "Legacy Circle"
+city = "Fairfax"
+state = "VA"
+zip_code = 22030
+
+print(f'Your shipping address is:\n\t\t{name}\n\t\t{building_number} {street_name}\n\t\t{city}, {state} {zip_code}')
